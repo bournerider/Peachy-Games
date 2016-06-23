1c1a3e49a56d27be7b89ba32b30a7fac77f14b97
jmcdaniel <josh.mcdaniel8520@gmail.com>
2016-06-23 17:15:50 -0500

    Added some sweet ass UI stuff

---
+++ SmashMOBA/Peach1/Assets/Scripts/ContinueToGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class ContinueToGame : MonoBehaviour
+{
+    void Start()
+    {
+       SceneManager.LoadScene("Fight Scene", LoadSceneMode.Single);
+    }
+}
+
