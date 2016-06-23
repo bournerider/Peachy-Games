13f045566c0f8ad657135f36daf4d0acd25dddcf
bournerider <logan.washbourne@okstate.edu>
2016-06-22 20:49:19 -0500

    Moving Platform With Collisions and Platform Waypoints

--- SmashMOBA/Peach1/Assets/Scripts/Controller2D.cs
+++ SmashMOBA/Peach1/Assets/Scripts/Controller2D.cs
@@ -17,7 +17,7 @@ public class Controller2D : RaycastController
         base.Start();
     }
 
-    public void Move(Vector3 velocity)
+    public void Move(Vector3 velocity, bool standingOnPlatform = false)
     {
         UpdateRaycastOrigins();
         collisions.Reset();
@@ -40,6 +40,11 @@ public class Controller2D : RaycastController
 
 
         transform.Translate(velocity);
+
+        if (standingOnPlatform)
+        {
+            collisions.below = true;
+        }
     }
 
 
@@ -57,6 +62,10 @@ public class Controller2D : RaycastController
 
             if (hit)
             {
+                if(hit.distance == 0)
+                {
+                    continue;
+                }
 
                 float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
 
