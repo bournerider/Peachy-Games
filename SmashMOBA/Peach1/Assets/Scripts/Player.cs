6956a073634606d93c83f587424aefdc2d1425b7
jmcdaniel <josh.mcdaniel8520@gmail.com>
2016-06-25 10:29:16 -0500

    Fuck the comment police. I decided against comments. Just adds messiness to the code. But I did add a speed modifier for running. Just hold shift!

--- SmashMOBA/Peach1/Assets/Scripts/Player.cs
+++ SmashMOBA/Peach1/Assets/Scripts/Player.cs
@@ -1,55 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//Required components as seen in the inspector
 [RequireComponent(typeof(Controller2D))]
 
 public class Player : MonoBehaviour
     {
     
-    //Initiate public variables - These can be changed in the inspector
-        public float jumpHeight = 4;
-        public float timeToJumpApex = .4f;
-        public float moveSpeed = 6;
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+    public float walkSpeed = 6;
+    public float sprintSpeed = 10;
     
-    //Initiate private variables - Can only be changed in the code
-        Vector3 velocity;
-        float gravity = -20;
-        float jumpVelocity = 8;
-        float velocityXSmoothing;
-        float accelerationTimeAirborne = .2f;
-        float accelerationTimeGrounded = .1f;
+    Vector3 velocity;
+    float moveSpeed;
+    float gravity = -20;
+    float jumpVelocity = 8;
+    float velocityXSmoothing;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
     
-    //
-        Controller2D controller;
-    
-    //
-        void Start()
-            {
-                controller = GetComponent<Controller2D>();
-                gravity = -(2 * jumpHeight) / (Mathf.Pow(timeToJumpApex, 2));
-                jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-                print("Gravity " + gravity + " Jump Velocity: " + jumpVelocity);
-            }
-    
-        void Update()
-            {
-                if (controller.collisions.above || controller.collisions.below)
-                    {
-                        velocity.y = 0;
-                    }
-        
-                Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        
-                if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
-                    {
-                        velocity.y = jumpVelocity;
-                    }
 
-                float targetVelocityX = input.x * moveSpeed;
-                velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
-                velocity.y += gravity * Time.deltaTime;
-                controller.Move(velocity * Time.deltaTime);
+    Controller2D controller;
     
-            }
-    }            
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+        gravity = -(2 * jumpHeight) / (Mathf.Pow(timeToJumpApex, 2));
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        print("Gravity " + gravity + " Jump Velocity: " + jumpVelocity);
+    }
+    
+    void Update()
+    {
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+    
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else
+        {
+            moveSpeed = walkSpeed;
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    
+    }
+}            
